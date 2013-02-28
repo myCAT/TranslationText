e3387563ef813e9f26b98317033e31dbe3c886d9
Nizar Ghoula <nizarghoula@gmail.com>
2013-02-28 15:41:42 +0100

    comment about reset words

--- src/java/org/olanto/TranslationText/client/BitextWidget.java
+++ src/java/org/olanto/TranslationText/client/BitextWidget.java
@@ -248,7 +248,7 @@ public class BitextWidget extends Composite {
         sourceTextArea.setText("");
         targetTextArea.setText("");
         Positions = null;
-        words = null;
+        words = null; // to reset the search tokens
         PreviousHitS.removeAllListeners();
         NextHitS.removeAllListeners();
         AlignS.removeAllListeners();
